8d6558b4de02fb0d535ca560cfa65599830e48aa
zikaoinfo <zikaoinfo@gmail.com>
2024-11-21 14:21:59 +0100

    feat(event-hub): add previous data stracking

--- ngx-event-hub-lib/projects/ngx-event-hub/src/lib/ngx-event-hub.service.ts
+++ ngx-event-hub-lib/projects/ngx-event-hub/src/lib/ngx-event-hub.service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NgxEventHubService {
-  eventsMap: Map<string,[(data?: {}) => void]> = new Map();
-  constructor() { }
-  on(eventName: string, callbackFn: (data?: {}) => void) {
-    let calbbacksForEvent = this.eventsMap.get(eventName)
-    if(calbbacksForEvent) {
-      calbbacksForEvent.push(callbackFn)
+  eventsMap: Map<string, [(data?: any, previousData?: any) => void]> =
+    new Map();
+  previousData: any = undefined;
+  constructor() {}
+  on(eventName: string, callbackFn: (data?: any, previousData?: any) => void) {
+    let calbbacksForEvent = this.eventsMap.get(eventName);
+    if (calbbacksForEvent) {
+      calbbacksForEvent.push(callbackFn);
     } else {
       calbbacksForEvent = [callbackFn];
       this.eventsMap.set(eventName, calbbacksForEvent);
     }
   }
-  
 
-  cast(eventName: string, data?: {} | undefined) {
+  cast(eventName: string, data?: any | undefined) {
     let callbacks = this.eventsMap.get(eventName) || [];
-
-    for(let callback of callbacks) {
-      callback?.(data);
+    for (let callback of callbacks) {
+      callback?.(data, this.previousData);
     }
+    this.previousData = data;
   }
 }
